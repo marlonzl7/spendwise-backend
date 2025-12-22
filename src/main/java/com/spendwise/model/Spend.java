311f042bb23e9f9ede878227d9c69665f07d7aec
regisdesouza <reginaldo.lima@sptech.school>
2025-12-22 20:25:55 -0300

    feat: Adicionada classe modelo que representa a transacao

---
+++ src/main/java/com/spendwise/model/Spend.java
@@ -0,0 +1,65 @@
+package com.spendwise.model;
+
+import com.spendwise.enums.Type;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Spend {
+    private int id;
+    private LocalDate date;
+    private String description;
+    private String category;
+    private BigDecimal value;
+    private Type type;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
